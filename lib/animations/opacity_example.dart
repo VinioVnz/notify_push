9efbdd6844e9bff0fd130c575978b45793dd090e
Vinícius Bornhofen <vinicius.bornhofen47@gmail.com>
2025-07-25 11:51:10 -0300

    animação

---
+++ lib/animations/opacity_example.dart
@@ -0,0 +1,37 @@
+/* import 'package:flutter/material.dart';
+
+class OpacityExample extends StatefulWidget {
+  const OpacityExample({super.key});
+
+  @override
+  State<OpacityExample> createState() => _OpacityExampleState();
+}
+
+class _OpacityExampleState extends State<OpacityExample> {
+  bool _visible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedOpacity(
+                opacity: _visible ? 1.0 : 00,
+                duration: Duration(seconds: 2),
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  color: ,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+ */
